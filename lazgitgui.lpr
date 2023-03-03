345c3fe6999957d06048a1305bb508187fd7bc63
Jesus Reyes A <jesusrmx@gmail.com>
2023-03-03 14:35:23 -0600

    Prepare execute git command

--- lazgitgui.lpr
+++ lazgitgui.lpr
@@ -9,13 +9,30 @@ uses
   {$IFDEF HASAMIGA}
   athreads,
   {$ENDIF}
-  Interfaces, // this includes the LCL widgetset
+  SysUtils, Interfaces, // this includes the LCL widgetset
   Forms, main, unitconfig
   { you can add units after this };
 
 {$R *.res}
 
 begin
+
+  if ParamCount=0 then begin
+    WriteLn(stdErr, 'A file or directory is needed in the command line');
+    exit;
+  end;
+
+  targetDir := ParamStr(ParamCount);
+  if FileExists(targetDir) then
+    targetDir := ExtractFilePath(targetDir)
+  else
+  if not DirectoryExists(targetDir) then begin
+    WriteLn(stdErr, 'No file or directory '+targetDir+' exists');
+    exit;
+  end;
+
+  targetDir := IncludeTrailingPathDelimiter(targetDir);
+
   RequireDerivedFormResource:=True;
   Application.Scaled:=True;
   Application.Initialize;
--- main.pas
+++ main.pas
@@ -5,8 +5,8 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  SynEdit, FileUtil, unitconfig;
+  Classes, SysUtils, UTF8Process, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, SynEdit, FileUtil, unitconfig;
 
 type
 
@@ -34,42 +34,78 @@ type
     Splitter1: TSplitter;
     Splitter2: TSplitter;
     txtDiff: TSynEdit;
+    procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
     fConfig: TConfig;
-    fTargetDir: String;
+    fGitCommand: string;
     procedure OpenDirectory(aDir: string);
+    procedure GitStatus(aDir: string);
   public
 
   end;
 
 var
   frmMain: TfrmMain;
+  targetDir: string = '';
 
 implementation
 
 {$R *.lfm}
 
+const
+  {$ifdef MsWindows}
+  EXE_EXTENSION = '.exe';
+  {$else}
+  EXE_EXTENSION = '';
+  {$endif}
+
 { TfrmMain }
 
 procedure TfrmMain.FormShow(Sender: TObject);
-var
-  aDir: string;
 begin
-  if ParamCount>0 then begin
-    aDir := ParamStr(ParamCount);
-    if not DirectoryExists(aDir) then begin
-       ShowMessage('Directory '+aDir+' does not exists');
-       exit;
-    end;
-    OpenDirectory(aDir);
+  OpenDirectory(targetDir);
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+
+  fGitCommand := fConfig.ReadString('git');
+  if (fGitCommand='') or (not FileExists(fGitCommand)) then begin
+    fGitCommand := FindDefaultExecutablePath('git' + EXE_EXTENSION);
   end;
+
 end;
 
 procedure TfrmMain.OpenDirectory(aDir: string);
 begin
-  fTargetDir := aDir;
-  ShowMessage('Updating repository data for: '+aDir);
+  GitStatus(aDir);
+end;
+
+procedure TfrmMain.GitStatus(aDir: string);
+var
+  process: TProcessUTF8;
+  Params: TStringlist;
+begin
+  //Git(fGitCommand, status -b -long --porcelain=2',
+
+  Params := TStringList.Create;
+  Params.Add('--C ');
+  Params.Add('"'+aDir+'"');
+  Params.Add('status');
+  Params.Add('-b');
+  Params.Add('--long');
+  Params.Add('--porcelain=2');
+  Process := TProcessUTF8.Create(nil);
+  try
+    Process.Executable := fGitCommand;
+    Process.Parameters.Assign(Params);
+    Process.Execute;
+  finally
+    Process.Free;
+    Params.Free;
+  end;
+
 end;
 
 end.
