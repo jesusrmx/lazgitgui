ed6c5d1f4f25e250d444bab8bd544a7932137e72
Jesus Reyes A <jesusrmx@gmail.com>
2023-04-04 01:10:33 -0600

    Added script for creating a sample repository

---
+++ scripts/samplerepo.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+REPO=$HOME/temp/repo.git
+
+makerepo () {
+  if [ ! -d repo.git ]; then
+    echo "Initializing a bare repository"
+    git init --bare repo.git
+  else
+    echo "The repository repo.git already exists"
+  fi
+}
+
+clonerepos () {
+  if [ ! -d gita ]; then
+    echo "Cloning sample repositories"
+    git clone file://$REPO gita
+    git clone file://$REPO gitb
+    git clone file://$REPO gitc
+  else
+    echo "The reposositories are already created"
+  fi
+}
+
+docommits () {
+
+  echo "Creating commits in $1"
+
+  FILE=notes_$1.txt
+  cd $1
+
+  if [ ! -f $FILE ]; then
+    echo "Initial text" > $FILE
+    git add $FILE
+    git commit -m "$1: Added file $FILE"
+  else
+    FILESIZE=$(stat -c%s "$FILE")
+    MSG=" - now with $FILESIZE bytes"
+  fi
+
+  for i in {1..10}; do
+    echo "Line $i$MSG" >> $FILE
+    git add $FILE
+    git commit -m "$1: added Line $i$MSG"
+    sleep 1
+  done
+
+  cd ..
+
+}
+
+makerepo
+clonerepos
+
+docommits gita
+docommits gitb
+docommits gitc
+
+
