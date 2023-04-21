434a85338bd483bc1cffeb124a103fb0a3acf8ff
Jesus Reyes A <jesusrmx@gmail.com>
2023-04-21 14:29:50 -0600

    Updated script

--- scripts/samplerepo.sh
+++ scripts/samplerepo.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-REPO=$HOME/temp/repo.git
+REPO=$HOME/temporal/repo.git
+MAXCOMMITS=5
+DRY=1
+
+GIT () {
+  if [ "$DRY" == "" ]; then
+    git $1 $2 $3 $4 $5 $6 $7 $8
+  else
+    echo "git $1 $2 $3 $4 $5 $6 $7 $8"
+  fi
+}
+
+CD () {
+  if [ "$DRY" == "" ]; then
+    cd $1
+  else
+    echo "cd $1"
+  fi
+}
 
 makerepo () {
   if [ ! -d repo.git ]; then
     echo "Initializing a bare repository"
-    git init --bare repo.git
+    GIT init --bare repo.git
   else
     echo "The repository repo.git already exists"
   fi
@@ -14,7 +32,7 @@ makerepo () {
 clonerepos () {
   if [ ! -d gita ]; then
     echo "Cloning sample repositories"
-    git clone file://$REPO gita
+    GIT clone file://$REPO gita
     #git clone file://$REPO gitb
     #git clone file://$REPO gitc
   else
@@ -27,26 +45,69 @@ docommits () {
   echo "Creating commits in $1"
 
   FILE=notes_$1.txt
-  cd $1
+  if [ "$DRY" == "" ]; then
+    BRANCH=$(git symbolic-ref --short HEAD)
+  else
+    BRANCH=XYZ
+  fi
+
+  CD $1
 
   if [ ! -f $FILE ]; then
     echo "Initial text" > $FILE
-    git add $FILE
-    git commit -m "$1: Added file $FILE"
+    GIT add $FILE
+    GIT commit -m "$1-$BRANCH: Added file $FILE"
   else
     FILESIZE=$(stat -c%s "$FILE")
     MSG=" - now with $FILESIZE bytes"
   fi
 
-  for i in {1..5}; do
+  for i in {1..$MAXCOMMITS}; do
     echo "Line $i$MSG" >> $FILE
-    git add $FILE
-    git commit -m "$1: added Line $i$MSG"
-    sleep 1
+    GIT add $FILE
+    GIT commit -m "$1-$BRANCH: added Line $i$MSG"
+    if [ "$DRY" == "" ]; then
+      sleep 1
+    else
+      echo "sleep 1"
+    fi
   done
 
-  cd ..
 
+  CD ..
+
+}
+
+createbranch () {
+  CD $1
+  GIT branch $2 master
+  GIT switch $2
+  CD ..
+}
+
+switchbranch () {
+  CD $1
+  GIT switch $2
+  CD ..
+}
+
+mergebranch () {
+  CD $1
+  GIT merge $2
+  CD ..
+}
+
+recreatemergetoright () {
+  docommits $1
+  docommits $1
+  docommits $1
+  createbranch $1 algo
+  docommits $1
+  switchbranch $1 master
+  docommits $1
+  mergebranch $1 algo
+  switchbranch $1 algo
+  docommits $1
 }
 
 if [ "$1" == "clean" ]; then
@@ -60,8 +121,9 @@ fi
 makerepo
 clonerepos
 
-docommits gita
+#docommits gita
 #docommits gitb
 #docommits gitc
 
+recreatemergetoright gita
 
