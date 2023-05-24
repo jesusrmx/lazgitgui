1dc3ef367f86efdee04339e98da42dd63f1b87f1
Jesus Reyes A <jesusrmx@gmail.com>
2023-05-23 19:35:43 -0600

    Log: Exclude stashes from the log list.

--- unitlogcache.pas
+++ unitlogcache.pas
@@ -190,8 +190,8 @@ type
 implementation
 
 const
-  LOG_CMD = '--pretty="format:%ct'#2'%P'#2'%H'#2'%an'#2'%ae'#2'%s'#2#3'" --all';
-  //LOG_CMD = '--pretty="format:%ct'#2'%P'#2'%H'#2'%an'#2'%ae'#2'%s'#2#3'" --exclude=refs/stash --all';
+  //LOG_CMD = '--pretty="format:%ct'#2'%P'#2'%H'#2'%an'#2'%ae'#2'%s'#2#3'" --all';
+  LOG_CMD = '--pretty="format:%ct'#2'%P'#2'%H'#2'%an'#2'%ae'#2'%s'#2#3'" --exclude=refs/stash --all';
   //LOG_CMD = '--pretty="format:%ct'#2'%P'#2'%H'#2'%an'#2'%ae'#2'%s'#2#3'" --branches --remotes --tags';
 
 
